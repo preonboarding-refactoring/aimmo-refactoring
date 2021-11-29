095e836bd0bcfffa9a70d3dc8bebee903f8a0bc8
Sungeheon <elon4856@gmail.com>
2021-11-29 16:58:26 +0900

    add: 몽고 db 설정

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import pymongo
+
+connection = pymongo.MongoClient()
+slqite_address = 'sqlite:////test.db'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-sqlite_db =SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = slqite_address
+sqlite_db = SQLAlchemy(app)
+
+test1 = connection.test
+
 
 @app.route("/", methods=["GET"])
 def hello():
