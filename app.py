b6f57130e9fe35a70612b211c96a06126f2eaacc
Sungeheon <elon4856@gmail.com>
2021-11-29 17:27:48 +0900

    feat: 로그인 기능 구현.

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+from flask_migrate import Migrate
+
 import pymongo
+import config
+
 
 connection = pymongo.MongoClient()
 slqite_address = 'sqlite:////test.db'
+migrate = Migrate()
 
 app = Flask(__name__)
+app.config.from_object(config)
 app.config['SQLALCHEMY_DATABASE_URI'] = slqite_address
 sqlite_db = SQLAlchemy(app)
+sqlite_db.init_app(app)
+migrate.init_app(app,sqlite_db)
+jwt = JWTManager(app)
 
 test1 = connection.test
 
