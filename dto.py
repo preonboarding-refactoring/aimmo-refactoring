8bb91a13d78ad15361c53e8902942e8988d2f2b0
Sungeheon <elon4856@gmail.com>
2021-12-02 10:30:35 +0900

    add: CommentDTO 추가.

--- dto.py
+++ dto.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class PostDTO:
     def __init__(self, title, content, category):
         self.title = title
@@ -7,7 +10,7 @@ class PostDTO:
 
 class PostResponseDTO:
     views: int
-    def __init__(self, title, content, category, author_id, created_at, comment, _id):
+    def __init__(self, title, content, category, author_id, created_at, comment, _id, modified_at):
         self._id = _id
         self.title = title
         self.content = content
@@ -15,3 +18,13 @@ class PostResponseDTO:
         self.author_id = author_id
         self.created_at = created_at
         self.comment = comment
+        self.modified_at = modified_at
+
+
+class CommentDTO:
+    created_at: datetime.datetime
+    author_id: int
+    post_id: str
+    def __init__(self, content, OID):
+        self.content = content
+        self.OID = OID
